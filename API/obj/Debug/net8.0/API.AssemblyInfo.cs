7ebf570580d57deb26cedbc18f5e661fb6a988a8
Kiran Tash Nariansamy <kirantashnariansamy@gmail.com>
2024-10-30 13:22:06 +0200

    Demo fixes for the return body of the order to represent order number and status

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b36d2be4b6fb16105e2eba13caab4a597027464")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a22ebf2b223ba95c7f428c156b6f0586e597b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
